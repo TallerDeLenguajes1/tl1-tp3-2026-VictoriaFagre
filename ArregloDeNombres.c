e7d994911889f83faa8a9524c42fa9804f4be2ff
Victoria Fagre <victoriafagre5@gmail.com>
2026-04-14 13:14:15 -0300

    1er commit: README y .gitignore creados luego de clonar

---
+++ ArregloDeNombres.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <string.h>
+
+#define TAM 50
+
+int main(){
+    char nombres[TAM];
+    puts("Ingrese el nombre de 5 personas:");
+    gets(nombres);
+
+
+    return 0;
+}
