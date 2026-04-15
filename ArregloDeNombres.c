22c0649c9dc9120a3b33dc359cdf75feed74e586
Victoria Fagre <victoriafagre5@gmail.com>
2026-04-15 17:12:16 -0300

    Parte 2_1_1 y 2_1_2 hechas

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -1,14 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include <string.h>
+#include<string.h>
 
-#define TAM 50
+void MostrarPersonas(char *vAux[], int tamaAux);
+void liberarMemoria(char *vAux[], int tamaAux);
 
-int main(){
-    char nombres[TAM];
-    puts("Ingrese el nombre de 5 personas:");
-    gets(nombres);
+int main (){
 
+    char nombre[50];
+    int tama = 5;
+    char *V[tama];
+    for(int i = 0; i < tama; i++){
+        printf("Ingrese el nombre de la persona %d:", i + 1);
+        scanf("%s",nombre);
+
+        int tamaNombre = strlen(nombre);
+        char *cadena = (char*)malloc(tamaNombre * sizeof(char) + 1); 
+        strcpy(cadena,nombre);
+
+        V[i] = cadena;
+    }
+
+    MostrarPersonas(V, tama);    
+    liberarMemoria(V, tama);
 
     return 0;
+}
+
+void MostrarPersonas(char *vAux[], int tamaAux){
+    printf("Los nombres alojados en el vector son:");
+    printf("[");
+    for(int i = 0; i < tamaAux; i++){
+        
+        if(i != 4){
+            printf("%s, ", vAux[i]);
+        }else{
+             printf("%s", vAux[i]);
+        }
+        
+    }
+    printf("]");
+}
+
+void liberarMemoria(char *vAux[], int tamaAux) {
+    for (int i = 0; i < tamaAux; i++) {
+        free(vAux[i]);
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tl1-tp3-2026-VictoriaFagre
+*2_1)Cuando uso los recursos de libreria de string es mas que nada para un "resize" uso el resize para que a partir de demasiados caracteres pueda achicar el tamaño comparando de lo que escribí del total y luego asignándolo a por memoria dinámica a lo justo y necesario a una cadena nueva que ya puede tomar por defecto por ser puntero a*
+
