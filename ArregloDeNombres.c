3311b11e2b6d277a75c5fb542d7fd65d78e1496a
Victoria Fagre <victoriafagre5@gmail.com>
2026-04-15 17:55:01 -0300

    hago 2_2_1: Búsqueda por ID

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
+
 
 void MostrarPersonas(char *vAux[], int tamaAux);
 void liberarMemoria(char *vAux[], int tamaAux);
+void BuscarNombre(int idAux, char *vAux[], int tamaAux);
 
 int main (){
-
+    srand(time(NULL));
     char nombre[50];
     int tama = 5;
     char *V[tama];
@@ -21,7 +24,12 @@ int main (){
         V[i] = cadena;
     }
 
-    MostrarPersonas(V, tama);    
+    MostrarPersonas(V, tama);
+    int ID;
+    ID = 1 + rand()%(7);
+    printf("\nID generado(para verificar): %d", ID);
+    BuscarNombre(ID,V,tama);
+    
     liberarMemoria(V, tama);
 
     return 0;
@@ -40,6 +48,24 @@ void MostrarPersonas(char *vAux[], int tamaAux){
         
     }
     printf("]");
+}
+
+/*En este branch, implementar una función BuscarNombre que reciba un número entero 
+positivo de ID (índice del vector) como entrada y Escribir por pantalla el nombre 
+ubicado en esa posición dentro del vector. Si el número ingresado no pertenece al 
+vector escribir “no se encontró el valor buscado”. */
+
+void BuscarNombre(int idAux, char *vAux[], int tamaAux){
+    int bandera = 0;
+    for(int i = 0; i < tamaAux; i++){
+        if(i == (idAux-1)){
+            printf("\nEl nombre ubicado en la posicion %d del vector es: %s", i+1,vAux[i]);
+            bandera = 1;
+        }
+    }
+    if(bandera == 0){
+        printf("\nNo se encontro el valor buscado");
+    }
 }
 
 void liberarMemoria(char *vAux[], int tamaAux) {
