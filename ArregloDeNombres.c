226e12e8292ac3ee1afbea8c5795f83195e3b367
Victoria Fagre <victoriafagre5@gmail.com>
2026-04-24 19:15:57 -0300

    2_2_2 hecho:modifico README de apunte y creo el char* BuscarNombre

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -3,6 +3,7 @@
 #include<string.h>
 
 void MostrarPersonas(char *vAux[], int tamaAux);
+char* BuscarNombre(char *claveAux, char *vAux[], int tamaAux);
 void liberarMemoria(char *vAux[], int tamaAux);
 
 int main (){
@@ -21,7 +22,25 @@ int main (){
         V[i] = cadena;
     }
 
-    MostrarPersonas(V, tama);    
+    MostrarPersonas(V, tama);
+
+    char *cadenaAux[50];
+    printf("\nEscriba una palabra clave para ser buscada en frase:");
+    scanf("%s",cadenaAux);
+
+    int tamCadena = strlen(cadenaAux);
+    char *clave1 = (char *) malloc (tamCadena * sizeof(char) + 1);
+
+    strcpy(clave1,cadenaAux);
+
+    char *resultado = BuscarNombre(clave1,V,tama);
+
+    if(resultado != NULL){
+        printf("La 1era palabra encontrada fue: %s\n", resultado);
+    }else{
+        printf("No se encontro nignuna palabra: %d",-1);
+    }
+
     liberarMemoria(V, tama);
 
     return 0;
@@ -40,6 +59,22 @@ void MostrarPersonas(char *vAux[], int tamaAux){
         
     }
     printf("]");
+}
+
+/*Implementar una nueva versión de la función BuscarNombre. 
+Esta versión deberá recibir una palabra clave como argumento y retornar el 
+primer nombre dentro del vector que contenga dicha palabra. Si no existe, 
+debería devolver -1. 
+(Investigá el uso de la función strstr para esta implementación.)*/
+
+char* BuscarNombre(char *claveAux, char *vAux[], int tamaAux){
+
+    for(int i = 0; i < tamaAux; i++){
+        if(strstr(vAux[i],claveAux) != NULL){
+            return vAux[i];
+        }
+    }
+    return NULL; //si no encontro nada que haga retornar, entonces la devolución final es NULL
 }
 
 void liberarMemoria(char *vAux[], int tamaAux) {
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # tl1-tp3-2026-VictoriaFagre
 *2_1)Cuando uso los recursos de libreria de string es mas que nada para un "resize" uso el resize para que a partir de demasiados caracteres pueda achicar el tamaño comparando de lo que escribí del total y luego asignándolo a por memoria dinámica a lo justo y necesario a una cadena nueva que ya puede tomar por defecto por ser puntero a*
 
+# Observación 24/04/26
+*Cuando trato de pasar un valor ingresado por teclado como cadena de caracteres, debe de ser indicado como puntero, ya que tiene varias letras que ocupan un lugar en memoria, no solo uno. Por eso debe de pasarse por referencia, si i arrreglo de caracteres YA ESTA AJUSTADO al tamaño de las letras a pasar debo de definir en el parámetro de la función receptora un char *claveAux; para que tome la dirección de memoria de clave1 y pueda trabajar con sus elementos*
+
